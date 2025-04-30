3f58e40904f6a2e9e32af0e9773f658eba0bde31
Kathy Trieu <38932563+itstrieu@users.noreply.github.com>
2025-04-29 23:38:57 -0700

    .

--- src/config/motion.py
+++ src/config/motion.py
@@ -14,9 +14,9 @@ FIN_SPEED = 85
 PWM_FREQ = 10000
 
 # Basic speeds & thresholds
-SPEED = 50
-CENTER_ROTATE_SPEED = 45
-SEARCH_ROTATE_SPEED = 45
+SPEED = 45
+CENTER_ROTATE_SPEED = 50
+SEARCH_ROTATE_SPEED = 50
 INTER_STEP_PAUSE = 0.5  # seconds, tune as needed (higher = slower)
 
 TARGET_AREA = 12000
