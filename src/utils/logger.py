a09e0138848196004553a6e06d77312cd9135a9a
Kathy Trieu <38932563+itstrieu@users.noreply.github.com>
2025-05-03 18:12:43 -0700

    fix: update Logger class to properly handle class-level logging

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -9,6 +9,23 @@ class Logger:
     It supports logging to both console and a log file.
     """
 
+    # Class-level logger instances
+    _loggers = {}
+
+    @classmethod
+    def get_logger(cls, name="default", log_level=logging.INFO, log_to_file=True):
+        """
+        Get or create a logger instance for the given name.
+
+        Parameters:
+        - name: Name of the logger (default "default"). Typically, use the component name (e.g., "motion", "sensor").
+        - log_level: The logging level (e.g., logging.INFO, logging.DEBUG). Defines which log messages are shown.
+        - log_to_file: Whether to log to a file (default True). If False, only console logging will occur.
+        """
+        if name not in cls._loggers:
+            cls._loggers[name] = cls(name, log_level, log_to_file)
+        return cls._loggers[name].logger
+
     def __init__(self, name="default", log_level=logging.INFO, log_to_file=True):
         """
         Initialize the Logger instance.
@@ -18,36 +35,22 @@ class Logger:
         - log_level: The logging level (e.g., logging.INFO, logging.DEBUG). Defines which log messages are shown.
         - log_to_file: Whether to log to a file (default True). If False, only console logging will occur.
         """
-        self.logger = logging.getLogger(
-            name
-        )  # TODO: Create a logger with the provided name
-        self.logger.setLevel(
-            log_level
-        )  # TODO: Set the log level to control message verbosity
+        self.logger = logging.getLogger(name)
+        self.logger.setLevel(log_level)
 
         # Stream handler (console logging)
-        console_handler = logging.StreamHandler()  # TODO: Handle log output to console
+        console_handler = logging.StreamHandler()
         console_formatter = logging.Formatter(
             "[%(asctime)s] [%(levelname)s] %(message)s"
-        )  # TODO: Format the log messages for better readability
-        console_handler.setFormatter(
-            console_formatter
-        )  # TODO: Attach the formatter to the handler
-        self.logger.addHandler(
-            console_handler
-        )  # TODO: Add the console handler to the logger
+        )
+        console_handler.setFormatter(console_formatter)
+        self.logger.addHandler(console_handler)
 
         # Optional: File handler (file logging)
         if log_to_file:
-            file_handler = logging.FileHandler(
-                f"{name}_log.txt"
-            )  # TODO: Log to file with the given file name
-            file_handler.setFormatter(
-                console_formatter
-            )  # TODO: Use the same log format for file output
-            self.logger.addHandler(
-                file_handler
-            )  # TODO: Add the file handler to the logger
+            file_handler = logging.FileHandler(f"{name}_log.txt")
+            file_handler.setFormatter(console_formatter)
+            self.logger.addHandler(file_handler)
 
     def get_logger(self):
         """
@@ -55,7 +58,6 @@ class Logger:
 
         This allows you to use the same logger across different modules.
         """
-        # TODO: Return the logger instance so it can be used elsewhere in the project
         return self.logger
 
     # Future Enhancements:
